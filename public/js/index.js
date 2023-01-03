9110145eab42d92fdee9ed4a6d169e23cfe32102
Hynn <choihwoong@me.com>
2023-01-03 13:58:43 +0900

    feat : added event of post in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,27 @@ ajax_get.addEventListener("click", () => {
     xhr.open("get", "http://localhost:3000/users")
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
     xhr.send()
+    console.log("open :", xhr.open)
+    console.log("open :", xhr.setRequestHeader)
+    console.log("open :", xhr.send)
 
     xhr.onload = () => {
         if(xhr.readyState === 4 && xhr.status === 200){
+            console.log(xhr.response)
+        }
+    }
+})
+
+ajax_post_addEventListener("click", () => {
+    xhr.open("post", "http://localhost:3000/users")
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xhr.send('userid=choihwoong&username="hynn')
+    console.log("open :", xhr.open)
+    console.log("open :", xhr.setRequestHeader)
+    console.log("open :", xhr.send)
+
+    xhr.onload = () => {
+        if (xhr.readyState === 4 && xhr.status === 200){
             console.log(xhr.response)
         }
     }
